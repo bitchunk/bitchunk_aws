6c2494c22e97dec819de61bb8c5b26201a83d135
oshiimizunohuta <g.ebodai@gmail.com>
2015-11-21 09:22:47 +0900

    GAセッティング

--- www/views/common/header.php
+++ www/views/common/header.php
@@ -33,21 +33,15 @@
 <script src="https://apis.google.com/js/platform.js" async defer>
 	{lang: 'ja'}
 </script>
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-46774451-2']);
-	_gaq.push(['_setDomainName', 'bitchunk.net']);
-	_gaq.push(['_setAllowLinker', true]);
-	_gaq.push(['_trackPageview']);
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	(function() {
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})(); 
+  ga('create', 'UA-46774451-6', 'auto');
+  ga('send', 'pageview');
+
 </script>
 <script>window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
