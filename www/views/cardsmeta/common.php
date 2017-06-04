e271df3c919301605829833c39ba45528c6e9694
oshiimizunohuta <g.ebodai@gmail.com>
2017-06-04 15:19:55 +0900

    ロゴアイコン画像のパス修正

--- www/views/cardsmeta/common.php
+++ www/views/cardsmeta/common.php
@@ -3,7 +3,7 @@
 <meta name="twitter:title" content="<?php echo SITE_NAME; ?>" />
 <meta name="twitter:description" content="<?php echo SITE_DESCRIPTION; ?>" />
 <meta name="twitter:creator" content="@shiftal_on" />
-<meta name="twitter:image:src" content="<?php echo IMAGES_PATH; ?>img/app_bit.png" />
+<meta name="twitter:image:src" content="<?php echo IMAGES_PATH; ?>/appicon/app_bit.png" />
 <meta name="twitter:domain" content="<?php echo HOST_NAME; ?>" />
 <link rel="shortcut icon" type="image/png" href="/img/favicon/favb.png" />
 <link rel="apple-touch-icon" href="/img/appicon/app_bit.png" />
