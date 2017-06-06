4797cd72175bc715693d8fa25c40ffa61b5ac16d
oshiimizunohuta <g.ebodai@gmail.com>
2017-06-06 23:52:23 +0900

    アドセンスを導入

--- www/views/common/footer.php
+++ www/views/common/footer.php
@@ -1,3 +1,16 @@
+	<section>
+		<article>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- コーポ・フッター -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5168032248020758"
+     data-ad-slot="3801576624"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+		</article>
+	</section>
 	<hr class="clear" />
 	<footer>
 		<div class="foot-layer">
