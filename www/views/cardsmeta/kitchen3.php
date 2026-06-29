b4fcebbbed2b9865d66b5027863b94d4e2e8bff4
bitchunk <shiftalow@mail.bitchunk.net>
2026-06-29 10:10:06 +0900

    kitchenmatchen3を仮追加

---
+++ www/views/cardsmeta/kitchen3.php
@@ -0,0 +1,20 @@
+<meta name="apple-mobile-web-app-title" content="KITCHEN MATCHEN 3">
+
+<meta property="og:title" content="KITCHEN MATCHEN 3" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="クッキング・マッチ3パズル　KITCHEN MATCHEN 3　体験版">
+<meta property="og:url" content="https://bitchunk.net/app/kitchen3" />
+<meta property="og:site_name" content="bitchunk" >
+<meta property="og:image" content="https://bitchunk.net/img/applist/konsairi_02.jpg" />
+
+<meta name="twitter:title" content="KITCHEN MATCHEN 3"/>
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:description" content="クッキング・マッチ3パズル　KITCHEN MATCHEN 3　体験版"/>
+<meta name="twitter:image:src" content="" />
+<meta name="twitter:site" content="@shiftal_on" />
+<meta name="twitter:creator" content="@shiftal_on" />
+<meta name="twitter:domain" content="<?php echo HOST_NAME; ?>" />
+
+<link rel="shortcut icon" type="image/png" href="/img/appicon/sicon.png" />
+<link rel="apple-touch-icon" href="/img/appicon/sicon.png" />
+<meta name="apple-mobile-web-app-title" content="KITCHENMATCHEN3" />
